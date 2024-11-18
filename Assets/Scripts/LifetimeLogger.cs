46050a1a39d04c3fc0670e28ae27fff6bbbc8320
Sean Tromans <seantromans@gmail.com>
2024-11-18 00:40:36 +0000

    Added utility to log the lifetime events of a component.

---
+++ Assets/Scripts/LifetimeLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LifetimeLogger : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log($"[{Time.frameCount} {GetInstanceID()}] Awake {transform.position} {transform.parent.name} {gameObject.activeInHierarchy}");
+        }
+        
+        private void OnEnable()
+        {
+            Debug.Log($"[{Time.frameCount} {GetInstanceID()}] OnEnable {transform.position} {transform.parent.name} {gameObject.activeInHierarchy}");
+        }
+        
+        private void Start()
+        {
+            Debug.Log($"[{Time.frameCount} {GetInstanceID()}] Start {transform.position} {transform.parent.name} {gameObject.activeInHierarchy}");
+        }
+        
+        private void OnDisable()
+        {
+            Debug.Log($"[{Time.frameCount} {GetInstanceID()}] OnDisable {transform.position} {transform.parent.name} {gameObject.activeInHierarchy}");
+        }
+        
+        private void OnDestroy()
+        {
+            Debug.Log($"[{Time.frameCount} {GetInstanceID()}] OnDestroy {transform.position} {transform.parent.name}");
+        }
+    }
+}
